0c374f3acf39f8668faa0623f1c964b0740a9c5e
Oscar Farfan <oscarfarfan92@gmail.com>
2022-07-07 23:02:54 -0500

    Se realizan las primeras 5 katas con sus respectivas pruebas funcionando

---
+++ src/main/java/katas/Kata4.java
@@ -0,0 +1,31 @@
+package katas;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import model.BoxArt;
+import model.MovieList;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/*
+    Goal: Retrieve id, title, and a 150x200 box art url for every video
+    DataSource: DataUtil.getMovieLists()
+    Output: List of ImmutableMap.of("id", "5", "title", "Bad Boys", "boxart": BoxArt)
+*/
+public class Kata4 {
+    public static List<Map> execute() {
+        List<MovieList> movieLists = DataUtil.getMovieLists();
+
+        return movieLists
+                .stream()
+                .map(element -> element.getVideos())
+                .flatMap(c -> c.stream())
+                .map(element -> ImmutableMap.of("id", element.getId(), "title", element.getTitle(), "boxart: ", new BoxArt(150, 200, element.getUri())))
+                .collect(Collectors.toList());
+
+        /*return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));*/
+    }
+}
---
+++ src/main/java/katas/Kata5.java
@@ -0,0 +1,27 @@
+package katas;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import model.BoxArt;
+import model.Movie;
+import model.MovieList;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+    Goal: Retrieve the largest rating using reduce()
+    DataSource: DataUtil.getMovies()
+    Output: Double
+*/
+public class Kata5 {
+    public static Double execute() {
+        List<Movie> movies = DataUtil.getMovies();
+
+        return movies
+                .stream()
+                .reduce((x, y) -> (x.getRating() > y.getRating()) ? x : y)
+                .get().getRating();
+    }
+}
